5b91903303df0c79c15459e21a025b12393611c0
DongsooWilliamKim <kdstheace@naver.com>
2023-01-22 22:30:48 +0900

    loginFormRecap
    loginFormRecap

---
+++ _posts/Frontend/NomadCoder/app.js
@@ -0,0 +1,31 @@
+const loginForm = document.querySelector("#login-form");
+const loginInput = loginForm.querySelector("input");
+const greeting = document.querySelector("#greeting");
+
+const HIDDEN_CLASSNAME = "hidden";
+const USERNAME_KEY = "username";
+
+function onLoginSubmit(event) {
+  event.preventDefault();
+  loginForm.classList.add(HIDDEN_CLASSNAME);
+
+  const username = loginInput.value;
+  localStorage.setItem(USERNAME_KEY, username);
+  printGreetings(username);
+}
+
+function printGreetings(username) {
+  greeting.innerText = `Hello, ${username}`;
+  greeting.classList.remove(HIDDEN_CLASSNAME);
+}
+
+const savedUsername = localStorage.getItem(USERNAME_KEY);
+
+if (savedUsername === null) {
+  // show the form
+  loginForm.classList.remove(HIDDEN_CLASSNAME);
+  loginForm.addEventListener("submit", onLoginSubmit);
+} else {
+  //show the question
+  printGreetings(savedUsername);
+}
