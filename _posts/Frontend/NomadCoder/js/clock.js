b27147ee71d721b2bff472634ad3fb04bb0e6ab1
DongsooWilliamKim <kdstheace@naver.com>
2023-01-23 01:37:03 +0900

    chrome main clone - clock
    chrome main clone - clock

--- _posts/Frontend/NomadCoder/js/clock.js
+++ _posts/Frontend/NomadCoder/js/clock.js
@@ -1,8 +1,12 @@
 const clock = document.querySelector("#clock");
 
-function sayHello() {
-  console.log("hello");
+function getClock() {
+  const date = new Date();
+  const hours = String(date.getHours()).padStart(2, "0");
+  const minutes = String(date.getMinutes()).padStart(2, "0");
+  const seconds = String(date.getSeconds()).padStart(2, "0");
+  clock.innerText = `${hours}:${minutes}:${seconds}`;
 }
 
-setTimeout(sayHello, 1000);
+setInterval(getClock, 1000);
 // clock.innerText = "10:00";
